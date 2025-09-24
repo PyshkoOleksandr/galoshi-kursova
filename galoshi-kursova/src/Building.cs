c3d8e009bc4e6b0246d4e576d1f787011dd82d89
Максим Коваль <makskoval007@gmail.com>
2025-09-24 20:18:25 +0300

    feat: add some methods and fields

--- galoshi-kursova/src/Building.cs
+++ galoshi-kursova/src/Building.cs
@@ -1,6 +1,47 @@
 ﻿namespace galoshi_kursova.src;
+using System.Numerics;
 
-internal class Building
+internal abstract class Building : GameObject
 {
+    private byte slotQuantity;
+    private byte occupiedSlots;
+    private byte slotCapacity;
+
+    public Building(Vector2 position, byte slotQuantity, int maxHealth)
+    {
+        this.position = position;
+        this.slotQuantity = slotQuantity;
+        this.occupiedSlots = 0;
+        this.maxHealth = maxHealth;
+        this.health = maxHealth;
+        this.isEnemy = false;
+    }
+
+    public bool hasFreeSlots()
+    {
+        return occupiedSlots < slotQuantity;
+    }
+
+    public void occupySlot()
+    {
+        if (hasFreeSlots())
+        {
+            occupiedSlots++;
+        }
+    }
+
+    public void freeSlot()
+    {
+        if (occupiedSlots > 0)
+        {
+            occupiedSlots--;
+        }
+    }
+
+    protected override void died()
+    {
+        // Logic for when the building is destroyed
+        Console.WriteLine("Building destroyed at position: " + position);
+    }
 
 }
