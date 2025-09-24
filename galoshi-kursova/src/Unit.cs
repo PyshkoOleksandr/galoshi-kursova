ed653124eb47ccd14e26d75a7a04223651af2388
Samovar Smoked <artemsergeevich2091@gmail.com>
2025-09-24 19:58:39 +0300

    ref!: add Player, InputManager
    BREAKING CHANGE: move SpawnPlayerUnits, SpawnEnemies from GameSession to Player

---
+++ galoshi-kursova/src/Unit.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace galoshi_kursova.src;
+
+internal abstract class Unit : GameObject
+{
+    protected Vector2 speed;
+    protected float attackRange;
+    protected int attackDamage;
+    protected float attackCooldownS;
+
+    protected virtual void move()
+    {
+        position += speed;
+    }
+
+    protected virtual void attack(GameObject target)
+    {
+        if (Vector2.Distance(position, target.position) <= attackRange)
+        {
+            target.takeDamage(attackDamage);
+        }
+    }
+}
